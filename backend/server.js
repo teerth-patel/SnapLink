e27ee220dbf7042f90f46e1243313408e6014d24
teerth-patel <teerthpatel1089@gmail.com>
2025-03-28 09:44:42 +0530

    Added Regiser API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const authRoute = require('./routes/authRoute')
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -8,6 +11,13 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB cluster: cluster0"); // add cluster name dynamically according to environment
+  })
+  .catch((err) => console.log("error connecting to mongo", err));
+app.use('/api', authRoute);
 app.get("/", (req, res) => {
   res.send("api get / works");
 });
